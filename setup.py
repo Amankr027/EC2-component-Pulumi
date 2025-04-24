41b6e5b8c07dcce93001dd852511f5d6ffe2ff99
Aman Kumar <97677738+Amankr027@users.noreply.github.com>
2025-04-24 14:58:44 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ec2_component',
+    name='ec2-component',
     version='0.1.0',
     packages=find_packages(),  
     install_requires=[
@@ -10,3 +10,4 @@ setup(
     ],
     python_requires='>=3.6',
 )
+
