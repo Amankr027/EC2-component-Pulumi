bbe479a6b983f34d6baf3cdc53d7245845cd38a8
Aman Kumar <97677738+Amankr027@users.noreply.github.com>
2025-04-24 14:45:11 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='ec2_component',
     version='0.1.0',
-    packages=find_packages(),
+    packages=find_packages(),  
     install_requires=[
-        'pulumi>=3.0.0',
-        'pulumi-aws>=5.0.0'
+        'pulumi',
+        'pulumi-aws',
     ],
     python_requires='>=3.6',
 )
